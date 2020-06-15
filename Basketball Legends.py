f3107f2ce29d73bf970829ad7f2ef8daded51a4a
Bruno Tosi <55807654+brunotosii@users.noreply.github.com>
2020-06-15 18:35:59 -0300

    Update Basketball Legends.py

--- Basketball Legends.py
+++ Basketball Legends.py
@@ -1,1 +1,28 @@
+import pygame
+import random
 
+#título do jogo
+titulo = ' Basketball Legends '
+
+#valores
+width = 600
+height = 450
+
+cesta_width = 120
+cesta_height = 500
+
+coin_height = 50
+coin_width = 50
+
+gap = 120
+
+title_screen = True
+game_over = False
+score = 0
+animacao_frame = 0
+
+velocidade = 9
+velocidade_jogo = 3
+gravity = 1
+
+FPS = 30
